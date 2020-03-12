49a5aab9cb3f1658c1a8702326746a730774cfca
Annaliisa Tongpattanakul <celticmatrix@hotmail.com>
2020-03-11 18:59:53 -0700

    created case switch function for main program/UI menu

--- assets/js/server.js
+++ assets/js/server.js
@@ -14,34 +14,77 @@ const connection = mysql.createConnection({
     password: "Vortexringstate2!",
     database: "companyDB" 
 });
+const main = async() =>{
 connection.connect(function(err) {
     if (err) {
       console.error("Having a Malfunction: " + err.stack);
       return;
     }
     console.log("Connection Success " + connection.threadId);
+    menuTasks();
   });
+}
+
+ main();
 
 // Initial User Interface
 // =============================================================================
-inquirer.prompt([
-    {
-        type: 'list',
-        message: "What Would you like to do?",
-        name: 'employees',
-        choices: 
-        [                 
-        'View All Employees',
-        'View All Employees By Department',
-        'View All Employees By Manager',
-        'Add Employee',
-        'Remove Employee',
-        'Update Employee Role',
-        'Update Employee Manager',
-        'Exit'
-        ],
-    },
-]) .then(answers => {
-    console.info('Testing interface, You would like to:', answers.employees);
-  });
+const menuChoices = [                 
+  'View All Employees',
+  'View All Employees By Department',
+  'View All Employees By Manager',
+  'Add Employee',
+  'Remove Employee',
+  'Update Employee Role',
+  'Update Employee Manager',
+  'View All Departments',
+  'Add Department',
+  'Remove Department',
+  'Exit'
+  ];
 
+const menuTasks = async () => {
+  let answer = await inquirer.prompt({
+    name: 'action',
+    type: 'list',
+    message: 'What would you like to do?',
+    choices: menuChoices
+  });
+  switch (answer.action){
+    case 'View All Employees':
+      viewEmployees();
+      break;
+    case 'View All Employees By Department':
+      viewByDept();
+      break;
+    case 'View All Employees By Manager':
+      viewByManager();
+      break;
+    case 'Add Employee':
+      addEmployee();
+      break;
+    case 'Remove Employee':
+      removeEmployee();
+      break;
+    case 'Update Employee Role':
+      updateRole();
+      break;
+    case 'Update Employee Manager':
+      updateManager();
+      break;
+    case 'View All Departments':
+      viewDepts();
+      break;
+    case 'Add Department':
+      addDept();
+      break;
+    case 'Remove Department':
+      removeDept();
+      break;
+    case 'Exit':
+      connection.end();
+      break;
+  };
+};
+
+              

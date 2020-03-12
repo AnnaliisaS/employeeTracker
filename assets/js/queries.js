0b3e021d03ffee737f57c22320b6babe233b6ba2
Annaliisa Tongpattanakul <celticmatrix@hotmail.com>
2020-03-11 20:04:39 -0700

    repetative guts of query functions - err/response, etc

--- assets/js/queries.js
+++ assets/js/queries.js
@@ -1,7 +1,111 @@
+// Dependencies
+// =============================================================================
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+const cTable = require('console.table');
+
+// Queries/functions
+// =============================================================================
+
+// View All Employees 
+const viewEmployees = () =>{
+   const query = '';
+   connection.query(query, (err,res) =>{
+       if (err) throw err;
+       console.table(res);
+       menuTask();
+   });
+};
+
+// View All Employees By Department
+const viewByDept = () =>{
+   const query = '';
+   connection.query(query, (err,res) =>{
+       if (err) throw err;
+       console.table(res);
+       menuTask();
+   });
+};
+
+// View All Employees By Manager
+const viewByManager = () =>{
+   const query = '';
+   connection.query(query, (err,res) =>{
+       if (err) throw err;
+       console.table(res);
+       menuTask();
+   });
+};
+
+// Add Employee
+const addEmployee = () =>{
+   const query = '';
+   connection.query(query, (err,res) =>{
+       if (err) throw err;
+       console.table(res);
+       menuTask();
+   });
+};
+
+// Remove Employee
+const removeEmployee = () =>{
+   const query = '';
+   connection.query(query, (err,res) =>{
+       if (err) throw err;
+       console.table(res);
+       menuTask();
+   });
+};
+
+// Update Employee Role
+const updateRole = () =>{
+   const query = '';
+   connection.query(query, (err,res) =>{
+       if (err) throw err;
+       console.table(res);
+       menuTask();
+   });
+};
+
+// Update Employee Manager
+const updateManager = () =>{
+   const query = '';
+   connection.query(query, (err,res) =>{
+       if (err) throw err;
+       console.table(res);
+       menuTask();
+   });
+};
+
+// View All Departments
+const viewDepts = () =>{
+   const query = '';
+   connection.query(query, (err,res) =>{
+       if (err) throw err;
+       console.table(res);
+       menuTask();
+   });
+};
+
+// Add Department
+const addDept = () =>{
+   const query = '';
+   connection.query(query, (err,res) =>{
+       if (err) throw err;
+       console.table(res);
+       menuTask();
+   });
+};
+
+//Remove Department
+const removeDept = () =>{
+   const query = '';
+   connection.query(query, (err,res) =>{
+       if (err) throw err;
+       console.table(res);
+       menuTask();
+   });
+};
 
 
-
-
-
-
-// module.exports = queries;
+module.exports = queries;
